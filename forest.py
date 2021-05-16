26a2b7175e4befbba45eb5e4adc3e5607889201c
Bridget Daly <bdaly223@gmail.com>
2021-05-16 13:12:00 -0500

    add forest

--- forest.py
+++ forest.py
@@ -1,16 +1,14 @@
 import sys
 import pandas as pd
 import numpy as np
-import statsmodels.api as sm
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, ShuffleSplit, GridSearchCV
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-
-np.set_printoptions(threshold=sys.maxsize)
+import xgboost as xgb
 
 # read/prep data
 dat = pd.read_csv("data/tokenized_reviews.csv")
@@ -25,51 +23,77 @@ X_train, X_test, y_train, y_test = train_test_split(dat.drop(columns=["popular"]
                                                     test_size = 0.15,
                                                     random_state = 229)
 
-# undersample train set
-majority_size = len(y_train[y_train==0])
-minority_size = len(y_train[y_train==1])
-majority_indices = y_train[y_train==0].index
-rng = np.random.default_rng(seed=229)
-drop_indices = rng.choice(majority_indices, majority_size-minority_size, replace=False)
-X_train = X_train.drop(drop_indices)
-y_train = y_train.drop(drop_indices)
-
 # feature subsets
 subset_a = ["user_reviews","days_since_review","user_rating","rating_diff"]
 subset_b = ["user_reviews","days_since_review","user_rating","rating_diff",
             "num_words","avg_word_len","avg_sent_len","pct_verbs",
             "pct_nouns","pct_adj","quote","sentiment"]
 
-# SUBSET A
-print("LOGISTIC REGRESSION SUBSET A")
 
-# train
-log_reg = sm.Logit(y_train, X_train[subset_a]).fit()
-print(log_reg.summary())
+# SUBSET A
+print("RANDOM FOREST SUBSET A")
+
+# model
+rf = xgb.XGBRegressor(objective='binary:logistic',
+                      eval_metric='error',
+                      seed=229,
+                      n_jobs=-1)
+
+# parameters to try
+parameters = {
+    'n_estimators': (50,100,1000),
+    'max_depth': (2,4,6),
+    'learning_rate': (0.01, 0.1, 0.3)
+}
+
+# perform validation
+gs_rf = GridSearchCV(rf,
+                     parameters,
+                     cv=ShuffleSplit(n_splits=1,
+                                     test_size=0.13,
+                                     random_state=229))
+gs_rf.fit(X_train[subset_a], y_train)
+print(gs_rf.cv_results_)
+print(gs_rf.best_params_)
 
 # predict
-predictions = log_reg.predict(X_test[subset_a])
+predictions = gs_rf.predict(X_test[subset_a])
 predictions = list(map(round,predictions))
 print(classification_report(y_test, predictions))
 print(confusion_matrix(y_test, predictions))
 print(roc_auc_score(y_test, predictions))
+
+# feature importance
+print(gs_rf.best_estimator_.feature_importances_)
+xgb.plot_importance(gs_rf.best_estimator_)
+plt.tight_layout()
+plt.savefig("rf_subseta.png")
 
 # SUBSET B
-print("\n\nLOGISTIC REGRESSION SUBSET B")
+print("\n\nRANDOM FOREST SUBSET B")
 
-# train
-log_reg = sm.Logit(y_train, X_train[subset_b]).fit()
-print(log_reg.summary())
+# model, parameters to try, gridsearch defined above
+
+# perform validation
+gs_rf.fit(X_train[subset_b], y_train)
+print(gs_rf.cv_results_)
+print(gs_rf.best_params_)
 
 # predict
-predictions = log_reg.predict(X_test[subset_b])
+predictions = gs_rf.predict(X_test[subset_b])
 predictions = list(map(round,predictions))
 print(classification_report(y_test, predictions))
 print(confusion_matrix(y_test, predictions))
 print(roc_auc_score(y_test, predictions))
 
+# feature importance
+print(gs_rf.best_estimator_.feature_importances_)
+xgb.plot_importance(gs_rf.best_estimator_)
+plt.tight_layout()
+plt.savefig("rf_subsetb.png")
+
 # BAG OF WORDS
-print("\n\nLOGISTIC REGRESSION BOW")
+print("\n\nRANDOM FOREST BOW")
 
 # pipeline
 bow_pipe = make_pipeline(
@@ -77,17 +101,17 @@ bow_pipe = make_pipeline(
                       transformers=[('countvectorizer',
                                      CountVectorizer(),
                                      'tokenized_words')]),
-    StandardScaler(with_mean=False),
-    LogisticRegression(penalty='l2',
-                       solver='saga',
-                       max_iter=1000,
-                       random_state=229,
-                       n_jobs=-1))
+    xgb.XGBRegressor(objective='binary:logistic',
+                     eval_metric='error',
+                     seed=229,
+                     n_jobs=-1))
 
 # parameters to try
 parameters = {
     'columntransformer__countvectorizer__max_features': (10000,50000),
-    'logisticregression__C': (10, 1, 0.01, 0.001)
+    'xgbregressor__n_estimators': (50,100,1000),
+    'xgbregressor__max_depth': (2,4,6),
+    'xgbregressor__learning_rate': (0.01, 0.1, 0.3)
 }
 
 # perform validation
@@ -108,15 +132,15 @@ print(confusion_matrix(y_test, predictions))
 print(roc_auc_score(y_test, predictions))
 
 # feature importance
-coefficients = gs_bow_pipe.best_estimator_.named_steps['logisticregression'].coef_[0]
-num_nonzero_coefs = len(np.where(abs(coefficients) > 0)[0])
-sorted_ind = np.argsort(abs(coefficients))[::-1][:num_nonzero_coefs]
-print(len(sorted_ind))
-print(np.take(coefficients,sorted_ind.tolist()))
-print(np.take(gs_bow_pipe.best_estimator_.named_steps['columntransformer'].get_feature_names(),sorted_ind.tolist()))
+sorted_ind = gs_bow_pipe.best_estimator_.named_steps['xgbregressor'].feature_importances_.argsort()[::-1]
+print(np.take(gs_bow_pipe.best_estimator_.named_steps['columntransformer'].get_feature_names(),sorted_ind.tolist())[:50])
+print(np.take(gs_bow_pipe.best_estimator_.named_steps['xgbregressor'].feature_importances_,sorted_ind.tolist())[:50])
+xgb.plot_importance(gs_bow_pipe.best_estimator_.named_steps['xgbregressor'], max_num_features=50)
+plt.tight_layout()
+plt.savefig("rf_bow.png")
 
 # TF-IDF
-print("\n\nLOGISTIC REGRESSION TF-IDF")
+print("\n\nRANDOM FOREST TF-IDF")
 
 # pipeline
 tf_pipe = make_pipeline(
@@ -124,16 +148,16 @@ tf_pipe = make_pipeline(
                       transformers=[('tfidfvectorizer',
                                      TfidfVectorizer(),
                                      'tokenized_words')]),
-    StandardScaler(with_mean=False),
-    LogisticRegression(penalty='l2',
-                       solver='saga',
-                       max_iter=1000,
-                       random_state=229,
-                       n_jobs=-1))
+    xgb.XGBRegressor(objective='binary:logistic',
+                     eval_metric='error',
+                     seed=229,
+                     n_jobs=-1))
 
 # parameters to try
 parameters = {
-    'logisticregression__C': (10, 1, 0.01, 0.001)
+    'xgbregressor__n_estimators': (50,100,1000),
+    'xgbregressor__max_depth': (2,4,6),
+    'xgbregressor__learning_rate': (0.01, 0.1, 0.3)
 }
 
 # perform validation
@@ -154,9 +178,9 @@ print(confusion_matrix(y_test, predictions))
 print(roc_auc_score(y_test, predictions))
 
 # feature importance
-coefficients = gs_tf_pipe.best_estimator_.named_steps['logisticregression'].coef_[0]
-num_nonzero_coefs = len(np.where(abs(coefficients) > 0)[0])
-sorted_ind = np.argsort(abs(coefficients))[::-1][:num_nonzero_coefs]
-print(len(sorted_ind))
-print(np.take(coefficients,sorted_ind.tolist()))
-print(np.take(gs_tf_pipe.best_estimator_.named_steps['columntransformer'].get_feature_names(),sorted_ind.tolist()))
+sorted_ind = gs_bow_pipe.best_estimator_.named_steps['xgbregressor'].feature_importances_.argsort()[::-1]
+print(np.take(gs_bow_pipe.best_estimator_.named_steps['columntransformer'].get_feature_names(),sorted_ind.tolist())[:50])
+print(np.take(gs_bow_pipe.best_estimator_.named_steps['xgbregressor'].feature_importances_,sorted_ind.tolist())[:50])
+xgb.plot_importance(gs_bow_pipe.best_estimator_.named_steps['xgbregressor'], max_num_features=50)
+plt.tight_layout()
+plt.savefig("rf_tfidf.png")
